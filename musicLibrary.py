1c727f2ce4dbb3f090790eeb141fe61b4fa924c8
SinhaWiz <151841974+SinhaWiz@users.noreply.github.com>
2024-07-14 08:26:41 +0600

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "dushtu kokil": "https://www.youtube.com/watch?v=5zU1qNYR2cw",
+    "sanam re": "https://www.youtube.com/watch?v=DS-raAyMxl4",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
+    "laage ura dhura" : "https://www.youtube.com/watch?v=f53nqIBzii8"
+}
